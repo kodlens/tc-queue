a689f9c4e4544913164f66c2c9eb9f79d13aab72
eshen <etiennewayne@gmail.com>
2026-03-19 08:15:33 +0800

    update panel side logo logic

--- resources/js/Components/PanelSideBarLogo.tsx
+++ resources/js/Components/PanelSideBarLogo.tsx
@@ -1,9 +1,9 @@
 const PanelSideBarLogo = ( { collapse } : { collapse?:boolean }) => {
   return (
     <div className="demo-logo-vertical my-10 mx-4">
-        <img width="150s" className='mx-auto' src="/img/tc-logo.png"></img>
+        <img width="100" className='mx-auto' src="/img/tc-logo.png"></img>
 
-        { !collapse && <div className='hidden md:block text-center text-white font-bold text-xl'>
+        { collapse && <div className='hidden md:block text-center text-white font-bold text-xl'>
             TANGUB CITY QUEUE MANAGEMENT SYSTEM
         </div> }
 
