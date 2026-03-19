1da9efb870320665f7b90b2150855f4aa641a849
eshen <etiennewayne@gmail.com>
2026-03-19 08:07:43 +0800

    updat repo

--- resources/js/Components/Reports/ProcessingTImePerService.tsx
+++ resources/js/Components/Reports/ProcessingTImePerService.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Table } from "antd";
 import axios from 'axios';
 import AverageProcesingTimeChart from './AverageProcesingTimeChart';
